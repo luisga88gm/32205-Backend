4f08f8f768e4b78040d7443e01cea9b2b4c44f03
Luis Gabriel García Mingall <90941271+luisga88gm@users.noreply.github.com>
2022-12-12 21:48:08 -0300

    Delete Ejercicios Clase 7 directory

--- Ejercicios Clase 7/http.js
+++
@@ -1,9 +0,0 @@
-const http = require('http')
-
-const server = http.createServer((request, response) => {
-    response.end("Saludos para <b>Luisito</b>")
-})
-
-server.listen(8080, () => {
-    console.log("Listening on port 8080...");
-})
