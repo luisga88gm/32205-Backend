f1a9c946e94550c385a9f5f65cdba53f6d78fc04
Luis Gabriel García Mingall <90941271+luisga88gm@users.noreply.github.com>
2023-04-08 19:20:47 -0300

    Delete Entrega Clase 38 directory

--- Entrega Clase 38/src/messages/routes/messages.router.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { authPolicies, authToken } from "../../utils/jwt.js";
-import { getChatPage } from "../controllers/messages.controller.js";
-
-const Router = express.Router();
-
-Router.get("/", authToken, authPolicies("USER"), getChatPage);
-
-export default Router;
